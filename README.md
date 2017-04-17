1f45928ed7d77f33b3c5f2381ef5ba1484c2424f
KillPippo <killpippo@hotmail.it>
2017-04-17 21:15:41 +0200

    added bower, client build forllowup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # BaseNodeTs
 Basic nodejs webapp written in typescript
+
+npm install -g gulp-cli
+npm install -g typescript
+npm install -g typings
+npm install -g bower
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,56 @@
 var gulp = require("gulp");
+var runSequence = require("run-sequence");
+var del = require("del");
+var mainBowerFiles = require("main-bower-files");
 var ts = require("gulp-typescript");
+
 var tsServerProject = ts.createProject("src/server/tsconfig.json");
 var tsClientProject = ts.createProject("src/client/tsconfig.json");
 
-gulp.task("buildServer", function () {
-    return tsServerProject.src()
-        .pipe(tsServerProject())
-        .js.pipe(gulp.dest("dist/server"));
+gulp.task("server:clean", function () {
+    return del("dist/server/**/*");
 });
 
-gulp.task("buildClient", function () {
-    return tsClientProject.src()
-        .pipe(tsClientProject())
-        .js.pipe(gulp.dest("dist/public"));
+gulp.task("client:clean", function () {
+    return del("dist/public/**/*");
+});
+
+gulp.task("client:copy_html", function () {
+    return gulp.src("src/client/**/*.html")
+        .pipe(gulp.dest("dist/public"));
+});
+
+gulp.task("client:dev:copy_libs", function () {
+    return gulp.src(mainBowerFiles({
+            base: 'src/client/bower_components'
+        }))
+        .pipe(gulp.dest("dist/public/libs"));
+});
+
+gulp.task("server:build", function (done) {
+    runSequence(
+        "server:clean",
+        function() {
+            tsServerProject.src()
+                .pipe(tsServerProject())
+                .js.pipe(gulp.dest("dist/server"));
+
+            done();
+        }
+    );
+});
+
+gulp.task("client:dev:build", function (done) {
+    runSequence(
+        "client:clean",
+        "client:dev:copy_libs",
+        "client:copy_html",
+        function () {
+            tsClientProject.src()
+                .pipe(tsClientProject())
+                .js.pipe(gulp.dest("dist/public/js"));
+
+            done();
+        }
+    );
 });
